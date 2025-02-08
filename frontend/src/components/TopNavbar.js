7bccb330f6a6eac7edb3e3424a41cfb63c8ad477
Tarishi Geetey <tarishi.geetey@gmail.com>
2025-02-08 14:24:58 +0530

    refactor : nav bar

--- frontend/src/components/TopNavbar.js
+++ frontend/src/components/TopNavbar.js
@@ -1,32 +1,45 @@
 import { NavLink } from 'react-router-dom';
 import { Container, Nav, Navbar, Col } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-const TopNavbar = () => {
-  return (
-    <Navbar className="container mb-auto" expand="sm"> {/* Remove fixed-top */}
-      <Container>
-        <Col className="text-start col-9">
-          <Navbar.Brand className="me-auto">
-            <Nav.Link as={NavLink} to="/" className="text-yellow">
-              <h3 id="brandName">muzik.</h3>
-            </Nav.Link>
-          </Navbar.Brand>
-        </Col>
+export default function TopNavbar() {
+    let [loggedInNavItemUrl, setLoggedInNavItemUrl] = useState('/');
+    let [loggedInNavItemName, setloggedInNavItemName] = useState('login');
+    const location = useLocation();
 
-        <Navbar.Toggle aria-controls="top-navbar" id="navbarToggler" />
-        <Navbar.Collapse id="top-navbar">
-          <Nav>
-            <Nav.Link as={NavLink} to="/home" className="mx-3 text-yellow">
-              home
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/about" className="mx-3 text-yellow">
-              about
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-};
+    useEffect(() => {
+        if (localStorage.getItem('spotifyUser')) {
+            setLoggedInNavItemUrl('/home');
+            setloggedInNavItemName('Home');
+        }
+    }, [location.pathname]);
 
-export default TopNavbar;
+    return (
+        <Navbar className="container mb-auto" expand="sm">
+            <Container>
+
+                <Col className="text-start col-9">
+                    <Navbar.Brand className="me-auto">
+                        <Nav.Link as={NavLink} to={loggedInNavItemUrl} className="text-white">
+                            <h3 id="brandName">MUZIK.</h3>
+                        </Nav.Link>
+                    </Navbar.Brand>
+                </Col>
+
+                <Navbar.Toggle aria-controls="top-navbar" id="navbarToggler" />
+                <Navbar.Collapse id="top-navbar">
+                    <Nav>
+                        <Nav.Link as={NavLink} to={loggedInNavItemUrl} className="mx-3 text-white">
+                            {loggedInNavItemName}
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/about" className="mx-3 text-white">
+                            about
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+
+            </Container>
+        </Navbar>
+    );
+}
